e0caf6839159fc3ed59d7d560ec71a1773e47736
Erik Sievers <erik.sievers@helo.se>
2021-05-30 20:01:46 +0200

    Added page rank for worker pool

--- LabD/page_rank.erl
+++ LabD/page_rank.erl
@@ -3,32 +3,43 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(page_rank).
--compile([export_all,nowarn_export_all]).
+
+-compile([export_all, nowarn_export_all]).
 
 %% Use map_reduce to count word occurrences
 
-map(Url,ok) ->
-    [{Url,Body}] = dets:lookup(web,Url),
-    Urls = crawl:find_urls(Url,Body),
-    [{U,1} || U <- Urls].
+map(Url, ok) ->
+    [{Url, Body}] = dets:lookup(web, Url),
+    Urls = crawl:find_urls(Url, Body),
+    [{U, 1} || U <- Urls].
 
 reduce(Url,Ns) ->
     [{Url,lists:sum(Ns)}].
 
 page_rank() ->
-    {ok,web} = dets:open_file(web,[{file,"web.dat"}]),
+    {ok, web} = dets:open_file(web, [{file, "web.dat"}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_seq(fun map/2, fun reduce/2, 
-			      [{Url,ok} || Url <- Urls]).
+                              [{Url, ok} || Url <- Urls]).
 
 page_rank_par() ->
-    dets:open_file(web,[{file,"web.dat"}]),
+    dets:open_file(web, [{file, "web.dat"}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_par(fun map/2, 32, fun reduce/2, 32, 
-			      [{Url,ok} || Url <- Urls]).
+                              [{Url, ok} || Url <- Urls]).
 
 page_rank_dist() ->
-    dets:open_file(web,[{file,"web.dat"}]),
+    dets:open_file(web, [{file, "web.dat"}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce_dist:map_reduce_dist(fun map/2, 32, fun reduce/2, 32, 
-			      [{Url,ok} || Url <- Urls]).
+
+page_rank_pool() ->
+    dets:open_file(web, [{file, "web.dat"}]),
+    Urls = dets:foldl(fun ({K, _}, Keys) -> [K | Keys] end,
+                      [],
+                      web),
+    map_reduce_working_pool:map_reduce_working_pool(fun map/2,
+                                                    32,
+                                                    fun reduce/2,
+                                                    32,
+                                                    [{Url, ok} || Url <- Urls]).
