21efb5268490af7bad151a2e82e0a3c3b01d2e77
Erik Sievers <erik.sievers@helo.se>
2021-05-30 18:56:07 +0200

    Cleaned the code

--- LabD/worker_pool.erl
+++ LabD/worker_pool.erl
@@ -20,11 +20,7 @@ create_collector(N, Callback) ->
     spawn_link(fun () ->
                        Callback !
                            {result,
-                            [receive
-                                 {Index, Res} ->
-                                     io:format("Index ~p: ~p\n", [Index, Res]),
-                                     Res
-                             end
+                            [receive {Index, Res} -> Res end
                              || Index <- lists:seq(0, N - 1)]}
                end).
 
@@ -32,14 +28,8 @@ worker_pool(Funs) ->
     CollectorPid = create_collector(length(Funs), self()),
     NodeCount = length(nodes()),
     {InitialFuns, LaterFuns} = lists:split(NodeCount, Funs),
-    % Spawn initial workers
-    io:format("Self() of worker_pool: ~p\n", [self()]),
-    io:format("Fun length: ~p\n", [length(Funs)]),
-    io:format("Initial length: ~p\n",
-              [length(InitialFuns)]),
-    io:format("Later length: ~p\n", [length(LaterFuns)]),
-    % Start server with remainder
     spawn_link(fun () ->
+                       % Spawn initial workers
                        [spawn_link(Node,
                                    worker_pool,
                                    worker_wrapper,
@@ -47,6 +37,7 @@ worker_pool(Funs) ->
                         || {{Fun, Node}, Index}
                                <- zip(zip(InitialFuns, nodes()),
                                       lists:seq(0, NodeCount))],
+                       % Start the pool for the rest
                        worker_queue(LaterFuns,
                                     length(InitialFuns),
                                     CollectorPid)
@@ -70,14 +61,9 @@ worker_queue([F | Funs], Index, CollectorPid) ->
                        worker_wrapper,
                        [F, Index, CollectorPid, self()])
     end,
-    io:format("Spawning index: ~p\n", [Index]),
-    io:format("CollectorPid: ~p\n", [CollectorPid]),
-    io:format("Self(): ~p\n", [self()]),
     worker_queue(Funs, Index + 1, CollectorPid).
 
 worker_wrapper(Fun, Index, CollectorPid, PoolPid) ->
-    io:format("Starting work on index ~p on node ~p \n",
-              [Index, node()]),
     % Do work
     Res = Fun(),
     % Send result
